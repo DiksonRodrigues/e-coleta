2ee082a40ff4591a1b814b761122fd17231aa878
Dikson Rodrigues <eoscreativetech@gmai.com>
2021-08-06 19:01:53 -0300

    ok

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
